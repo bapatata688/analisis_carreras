2fcedf669cf611bd3eba92ff81c5f46dcb1383bf
bapatata688 <b0ldm6diego@gmail.com>
2026-04-23 17:22:51 -0600

    reportes hechos

--- reporte_1/personas_por_ciudad.py
+++ reporte_1/personas_por_ciudad.py
@@ -1,12 +1,15 @@
 import csv
 
 # diccionario con llave id_trabajadores que sera una lista
-encuestas = {"id_trabajadores": []}
+encuestas = {}
 with open("../dataset_10000_personas.csv", "r", encoding="utf-8") as encuesta:
-    id_encuestados = csv.DictReader(encuesta)
-    # recorrer la id en el csv y agregarlo al diccionario en id_trabajadores
-    for encuestado in id_encuestados:
-        id_actual = encuestado["id"]
-        encuestas["id_trabajadores"].append(id_actual)
-total_identidades = len(encuestas["id_trabajadores"])
-print(f"Total de identidades: {total_identidades}")
+    lector = csv.DictReader(encuesta)
+    # si la ciudad no esta en el diccionario encuestas se crea como lista
+    for datos in lector:
+        ciudad = datos["ciudad"]
+        if ciudad not in encuestas:
+            encuestas[ciudad] = []
+        encuestas[ciudad].append(datos)
+# se recorre llave y valor de encuestas y se muestra
+for llave, valor in encuestas.items():
+    print(f"{llave}: {len(valor)}")
